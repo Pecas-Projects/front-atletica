3cc35bde665740ad46902ad51708f833d44ecaaf
biacalazans <biacalazans@users.noreply.github.com>
2020-09-28 20:39:41 -0300

    Começando projeto

---
+++ src/Routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Perfil from "./Pages/Perfil"
+
+const Routes = () => (
+    <Switch>
+        <Route>
+            <Route exact path="/" component={Perfil} />
+        </Route>
+    </Switch>
+)
