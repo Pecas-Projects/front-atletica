2b9ea8fe8be32034e1ecf35493ed6575bed4046c
biacalazans <biacalazans@users.noreply.github.com>
2020-10-05 20:47:08 -0300

    Update NavBar

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import clsx from "clsx";
 import { makeStyles, useTheme, fade } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
@@ -11,12 +11,19 @@ import MenuIcon from "@material-ui/icons/Menu";
 import { ChevronLeft } from "@material-ui/icons";
 import ChevronRight from "@material-ui/icons/ChevronRight";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
 import InputBase from "@material-ui/core/InputBase";
+import Home from "../../assets/imagem/home.svg"
+import Feed from "../../assets/imagem/today (1).svg"
+import Calendario from "../../assets/imagem/calendar-today.svg"
+import Trofeu from "../../assets/imagem/trophy.svg"
+import Bag from "../../assets/imagem/shopping-bag.svg"
+import More from "../../assets/imagem/more-vertical-alt.svg"
+import LogOut from "../../assets/imagem/log-out.svg"
+import SearchIcon from "@material-ui/icons/Search";
 import "./NavBar.css";
+import { Grid } from "@material-ui/core";
 
 const drawerWidth = 200;
 
@@ -120,12 +127,33 @@ const useStyles = makeStyles((theme) => ({
       width: "20ch",
     },
   },
+  listItem: {
+    backgroundColor: "#020431",
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+
+
 }));
 
 export default function MiniDrawer() {
   const classes = useStyles();
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -135,80 +163,283 @@ export default function MiniDrawer() {
     setOpen(false);
   };
 
+
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
+
+    <>
+      <div className={classes.sectionDesktop}>
+
+        <div className={classes.root}>
+          <CssBaseline />
+
+          <AppBar
+            position="fixed"
+            className={clsx(classes.appBar, {
+              [classes.appBarShift]: open,
             })}
           >
-            <MenuIcon />
-          </IconButton>
-          <h1 className="logo">OLYMPOS</h1>
-          <InputBase
-            placeholder="Pesquise uma atlética"
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                className={clsx(classes.menuButton, {
+                  [classes.hide]: open,
+                })}
+              >
+                <MenuIcon />
+              </IconButton>
+              <h1 className="logo">OLYMPOS</h1>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Pesquise uma atletica"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </div>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            containerStyle={{ background: "#020431" }}
+            variant="permanent"
+            className={clsx(classes.drawer, {
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            })}
             classes={{
-              root: classes.inputRoot,
-              input: classes.inputInput,
+              paper: clsx({
+                [classes.drawerOpen]: open,
+                [classes.drawerClose]: !open,
+              }),
             }}
-            inputProps={{ "aria-label": "search" }}
-          />
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        containerStyle={{ background: "#020431" }}
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose} style={{ color: "white" }}>
-            {theme.direction === "rtl" ? <ChevronRight /> : <ChevronLeft />}
-          </IconButton>
+          >
+            <div className={classes.toolbar}>
+              <IconButton onClick={handleDrawerClose} style={{ color: "white" }}>
+                {theme.direction === "rtl" ? <ChevronRight /> : <ChevronLeft />}
+              </IconButton>
+            </div>
+
+            <Grid container spacing={1} direction='collumn' >
+
+              <Grid item xs={8} style={{ marginTop: 80, marginBottom: 100 }}>
+
+                <List >
+
+                  <ListItem button className="listItem">
+                    <ListItemIcon  >
+                      <img src={Home} alt="home" />
+                    </ListItemIcon>
+                    <ListItemText className="item" primary="Home" />
+                  </ListItem>
+
+
+                  <ListItem button >
+                    <ListItemIcon>
+                      <img src={Feed} alt="feed" />
+                    </ListItemIcon>
+                    <ListItemText className="item" primary="Feed" />
+                  </ListItem>
+
+                  <ListItem button >
+                    <ListItemIcon>
+                      <img src={Calendario} alt="calendario" />
+                    </ListItemIcon>
+                    <ListItemText className="item" primary="Calendário" />
+                  </ListItem>
+
+                  <ListItem button >
+                    <ListItemIcon>
+                      <img src={Trofeu} alt="times" />
+                    </ListItemIcon>
+                    <ListItemText className="item" primary="Times" />
+                  </ListItem>
+
+                  <ListItem button >
+                    <ListItemIcon>
+                      <img src={Bag} alt="produtos" />
+                    </ListItemIcon>
+                    <ListItemText className="item" primary="Produtos" />
+                  </ListItem>
+                </List>
+
+              </Grid>
+
+              <Grid item xs={6}>
+
+                <Grid container justify="flex-end" alignItems="flex-end" alignContent="flex-end">
+
+                  <ListItem button >
+                    <ListItemIcon>
+                      <img src={More} alt="configurações" />
+                    </ListItemIcon>
+                    <ListItemText className="item" primary="Configurações" />
+                  </ListItem>
+
+                  <ListItem button >
+                    <ListItemIcon>
+                      <img src={LogOut} alt="logout" />
+                    </ListItemIcon>
+                    <ListItemText className="item" primary="Logout" />
+                  </ListItem>
+                </Grid>
+
+              </Grid>
+
+
+            </Grid>
+
+
+          </Drawer>
         </div>
+      </div>
 
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon style={{ color: "white" }}>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+      {/*
+      
+      
+      
+      MOBILE 
+      
+      
+      
+      */}
 
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon style={{ color: "white" }}>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-    </div>
+
+      <div className={classes.sectionMobile}>
+
+        <CssBaseline />
+
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <h1 className="logoMobile">OLYMPOS</h1>
+
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Pesquise uma atletica"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          containerStyle={{ background: "#020431" }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          classes={{
+            paper: classes.drawerOpen,
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose} style={{ color: "white" }}>
+              {theme.direction === "rtl" ? <ChevronRight /> : <ChevronLeft />}
+            </IconButton>
+          </div>
+
+          <Grid container spacing={1} direction='collumn'  >
+
+            <Grid item xs={8} style={{ marginBottom: 50 }}>
+
+              <List >
+
+                <ListItem button className="listItem">
+                  <ListItemIcon  >
+                    <img src={Home} alt="home" />
+                  </ListItemIcon>
+                  <ListItemText className="item" primary="Home" />
+                </ListItem>
+
+
+                <ListItem button >
+                  <ListItemIcon>
+                    <img src={Feed} alt="feed" />
+                  </ListItemIcon>
+                  <ListItemText className="item" primary="Feed" />
+                </ListItem>
+
+                <ListItem button >
+                  <ListItemIcon>
+                    <img src={Calendario} alt="calendario" />
+                  </ListItemIcon>
+                  <ListItemText className="item" primary="Calendário" />
+                </ListItem>
+
+                <ListItem button >
+                  <ListItemIcon>
+                    <img src={Trofeu} alt="times" />
+                  </ListItemIcon>
+                  <ListItemText className="item" primary="Times" />
+                </ListItem>
+
+                <ListItem button >
+                  <ListItemIcon>
+                    <img src={Bag} alt="produtos" />
+                  </ListItemIcon>
+                  <ListItemText className="item" primary="Produtos" />
+                </ListItem>
+              </List>
+
+            </Grid>
+
+            <Grid item xs={6}>
+
+              <Grid container justify="flex-end" alignItems="flex-end" alignContent="flex-end">
+
+                <ListItem button >
+                  <ListItemIcon>
+                    <img src={More} alt="configurações" />
+                  </ListItemIcon>
+                  <ListItemText className="item" primary="Configurações" />
+                </ListItem>
+
+                <ListItem button >
+                  <ListItemIcon>
+                    <img src={LogOut} alt="logout" />
+                  </ListItemIcon>
+                  <ListItemText className="item" primary="Logout" />
+                </ListItem>
+              </Grid>
+
+            </Grid>
+
+
+          </Grid>
+
+
+        </Drawer>
+
+      </div>
+
+    </>
   );
 }
