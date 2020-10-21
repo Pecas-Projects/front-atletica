e0d7b9ed9863fe0b9566dfaf94724ff60a3c8305
AmadoMaria <mariantoniamado@hotmail.com>
2020-10-21 18:46:01 -0300

    Alteração página de produto
    - Retirando grids não utilizados
    - Centralizando cards e removendo paper,  no mobile

--- src/Pages/Produtos/Components/CardProduto.js
+++ src/Pages/Produtos/Components/CardProduto.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import { Grid, Card, CardActionArea, CardContent, CardMedia, Typography } from "@material-ui/core";
-import "../styles.css"
-
+import React from "react";
+import {
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
+import "../styles.css";
 
 export default function CardProduto(props) {
+  const { item } = props;
 
-    const { item } = props
+  return (
+    <>
+      <Grid item xs={12} sm={4} style={{ marginBottom: 25 }}>
+        <Card style={{ backgroundColor: "#73797D", maxWidth: 300 }}>
+          <CardMedia
+            image={item.imagem}
+            title={item.titulo}
+            style={{ height: 300 }}
+          />
 
-    return (
+          <CardContent>
+            <Typography gutterBottom style={{ color: "white", fontSize: 18 }}>
+              {item.titulo}
+            </Typography>
 
-        <Grid item xs={12} sm={4} style={{ marginBottom: 25 }}>
-            <Card style={{ backgroundColor: "#73797D", maxWidth: 300 }}>
-                <CardActionArea>
-                    <CardMedia image={item.imagem} title="foto do produto" style={{ height: 300 }} />
+            <Typography variant="body2" color="textSecondary" component="p">
+              {item.descricao}
+            </Typography>
 
-                    <CardContent>
-                        <Typography gutterBottom style={{ color: "white", fontSize: 18 }}>
-                            {item.titulo}
-                        </Typography>
-
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            {item.descricao}
-                        </Typography>
-
-                        <p className="cardPrice" >R$ {item.preco}</p>
-
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </Grid>
-
-    );
+            <p className="cardPrice">R$ {item.preco}</p>
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
+  );
 }
