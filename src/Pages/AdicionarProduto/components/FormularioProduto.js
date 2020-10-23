eb3bc789a7fdf0f0535eed780918db0eb988e1fc
AmadoMaria <mariantoniamado@hotmail.com>
2020-10-22 21:32:34 -0300

    Adicionando botão de upload de imagem

--- src/Pages/AdicionarProduto/components/FormularioProduto.js
+++ src/Pages/AdicionarProduto/components/FormularioProduto.js
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { AvForm, AvField } from "availity-reactstrap-validation";
-import { Grid, Typography, Paper, Button } from "@material-ui/core";
+import { Grid, Typography, Paper, Button, IconButton } from "@material-ui/core";
 import AddFile from "../../../assets/imagem/file-add.svg";
 
+const useStyles = makeStyles((theme) => ({
+  input: {
+    display: "none",
+  },
+}));
+
+function UploadButtons(props) {
+  const classes = useStyles();
+//   if (props.imagem !== null) {
+//     return <img src={props.imagem} alt="adicionar imagem" />;
+//   } else {
+    return (
+      <div>
+        <input
+          accept="image/*"
+          className={classes.input}
+          id="contained-button-file"
+          multiple
+          type="file"
+          accept="image/png, image/jpeg"
+          onClick={props.setImagem(accept)}
+        />
+        <label htmlFor="contained-button-file">
+          <IconButton
+            color="primary"
+            aria-label="upload picture"
+            component="span"
+          >
+            <img src={AddFile} alt="adicionar imagem" />
+          </IconButton>
+        </label>
+      </div>
+    );
+//   }
+}
+
 export default function FormularioProduto() {
+  const [imagem, setImagem] = useState();
+
   return (
     <>
       <Grid container justify="center" style={{ marginBottom: 25 }}>
@@ -16,10 +55,15 @@ export default function FormularioProduto() {
             <AvForm>
               <Grid container spacing={5} style={{ paddingTop: 20 }}>
                 <Grid item xs={4}>
-                  <Typography>Adicione uma Imagem</Typography>
+                  <p>Adicione uma Imagem</p>
                   <Paper style={{ backgroundColor: "#636363" }}>
-                    <Grid container justify="center" style={{ height: 140 }}>
-                      <img src={AddFile} alt="adicionar imagem" />
+                    <Grid
+                      container
+                      justify="center"
+                      alignContent="center"
+                      style={{ height: 140, marginTop: -7 }}
+                    >
+                      <UploadButtons />
                     </Grid>
                   </Paper>
                 </Grid>
@@ -37,7 +81,7 @@ export default function FormularioProduto() {
                       minLength: { value: 2, errorMessage: "Nome inválido" },
                       maxLength: { value: 45, errorMessage: "Nome inválido" },
                     }}
-                    style={{color:"E2E2E2"}}
+                    style={{ color: "E2E2E2" }}
                   />
                   <AvField
                     name="nome"
@@ -63,7 +107,11 @@ export default function FormularioProduto() {
                     </AvField>
                   </Grid>
                   <Grid item xs={6}>
-                    <Grid container justify="flex-end" style={{ paddingRight: 20 }}>
+                    <Grid
+                      container
+                      justify="flex-end"
+                      style={{ paddingRight: 20 }}
+                    >
                       <Button
                         style={{
                           background: "#DB4922",
