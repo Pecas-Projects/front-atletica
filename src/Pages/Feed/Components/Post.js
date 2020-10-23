0632fcb0fac15911f7c60cc6d10411364f5a9b47
biacalazans <biacalazans@users.noreply.github.com>
2020-10-23 17:38:24 -0300

    Atualização post

--- src/Pages/Feed/Components/Post.js
+++ src/Pages/Feed/Components/Post.js
@@ -58,12 +58,7 @@ export default function Post() {
                     image={fotoPost}
                     title="Foto do produto"
                 />
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        This impressive paella is a perfect party dish and a fun meal to cook together with your
-                        guests. Add 1 cup of frozen peas along with the mussels, if you like.
-          </Typography>
-                </CardContent>
+
                 <CardActions disableSpacing>
                     <IconButton
                         className={clsx(classes.expand, {
