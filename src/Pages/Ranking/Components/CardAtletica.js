a819a43018d5ba608f82629984845abc629d52a5
fernandanlisboa <fernandanlisboa@gmail.com>
2020-11-03 20:14:48 -0300

    Criação do Card de cada atlética

---
+++ src/Pages/Ranking/Components/CardAtletica.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import { AvForm, AvField } from "availity-reactstrap-validation";
+import { Grid, Typography, Paper, Button, IconButton } from "@material-ui/core";
+
+function CardAtletica(){
+
+    return(
+        <>
+        </>
+    );
+}
+
+export default CardAtletica;
