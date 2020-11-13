a0c1310eb2f397e4b3797f3ff25b398774063f7c
biacalazans <biacalazans@users.noreply.github.com>
2020-11-13 19:47:50 -0300

    Cards Atleta
    Prontos com dialog de confirmação e alert

--- src/Pages/AdicionarModalidade/Components/CardAtletaAdd.js
+++ src/Pages/AdicionarModalidade/Components/CardAtletaAdd.js
@@ -1,12 +1,48 @@
-import React from "react";
-import { Grid, Paper, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import { Grid, Paper, Typography, Snackbar, Dialog, DialogActions, DialogTitle, Button } from "@material-ui/core";
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function CardAtletaAdd(props) {
 
+    const [openAdd, setOpenAdd] = useState(false)
+    const [openAdicionar, setOpenAdicionar] = useState(false)
+
+
+    const handleCloseAdd = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenAdd(false);
+    };
+
+    const handleOpenAdd = () => {
+        setOpenAdicionar(false)
+        setOpenAdd(true)
+    }
+
+    const handleOpenAdicionar = () => {
+        setOpenAdicionar(true)
+    }
+
+    const handleCloseAdicionar = () => {
+        setOpenAdicionar(false)
+    }
+
     return (
         <>
+            <Snackbar open={openAdd} autoHideDuration={4000} onClose={handleCloseAdd}>
+                <Alert onClose={handleCloseAdd} severity="success">
+                    Atleta adicionado com sucesso!
+                </Alert>
+            </Snackbar>
+
             <Grid item xs={12} sm={3}>
 
                 <Paper style={{ padding: "4%", backgroundColor: "#D2CFE5" }}>
@@ -26,7 +62,7 @@ export default function CardAtletaAdd(props) {
 
                             <Grid container justify='flex-end'>
 
-                                <IconButton style={{ marginTop: -15 }}>
+                                <IconButton onClick={handleOpenAdicionar} style={{ marginTop: -15 }}>
                                     <AddIcon style={{ color: "green" }} />
                                 </IconButton>
 
@@ -39,6 +75,27 @@ export default function CardAtletaAdd(props) {
                 </Paper>
 
             </Grid>
+
+            <div>
+
+                <Dialog
+                    open={openAdicionar}
+                    onClose={handleCloseAdicionar}
+
+                >
+                    <DialogTitle id="alert-dialog-excluir">{"Tem certeza que deseja adicionar esse atleta a modalidade?"}</DialogTitle>
+
+                    <DialogActions>
+                        <Button onClick={handleOpenAdd} color="primary">
+                            Adicionar
+        </Button>
+                        <Button variant='outlined' onClick={handleCloseAdicionar} color="primary" autoFocus>
+                            Cancelar
+        </Button>
+                    </DialogActions>
+                </Dialog>
+
+            </div>
         </>
     );
 }
--- src/Pages/AdicionarModalidade/Components/CardAtletaDelete.js
+++ src/Pages/AdicionarModalidade/Components/CardAtletaDelete.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
-import { Grid, Paper, Typography, Dialog, DialogActions, DialogTitle, Button } from "@material-ui/core";
+import { Grid, Paper, Typography, Dialog, DialogActions, DialogTitle, Button, Snackbar } from "@material-ui/core";
 import IconButton from '@material-ui/core/IconButton';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import MuiAlert from '@material-ui/lab/Alert';
+
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function CardAtletaDelete(props) {
 
     const { atleta, index, DeleteAtleta } = props
 
     const [openExcluir, setOpenExcluir] = useState(false)
+    const [openExcluido, setOpenExcluido] = useState(false)
 
     const Deletar = () => {
 
         DeleteAtleta(index)
         setOpenExcluir(false)
+        setOpenExcluido(true)
     };
 
     const handleExcluir = () => {
@@ -23,9 +31,22 @@ export default function CardAtletaDelete(props) {
         setOpenExcluir(false);
     };
 
+    const handleCloseExcluido = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenExcluido(false);
+    };
+
 
     return (
         <>
+            <Snackbar open={openExcluido} autoHideDuration={4000} onClose={handleCloseExcluido}>
+                <Alert onClose={handleCloseExcluido} severity="success">
+                    Atleta excluido com sucesso!
+                </Alert>
+            </Snackbar>
             <Grid item xs={12} sm={3}>
 
                 <Paper style={{ padding: "4%", backgroundColor: "#D2CFE5" }}>
