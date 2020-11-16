23651f441e48b0b79e2316b2c6a3d529ad8d05e2
biacalazans <biacalazans@users.noreply.github.com>
2020-11-16 16:21:12 -0300

    Atualização footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { Typography } from '@material-ui/core'
+import CopyrightIcon from '@material-ui/icons/Copyright';
+
 
 export default function Footer() {
   return (
-    <footer
-      style={{
-        textAlign: "center",
-        backgroundColor: "#020431",
-        height: 150,
-        width: "100%",
-      }}
-    >
-      <p>Pecas brabas</p>
+
+    <footer style={{
+      textAlign: "center",
+      backgroundColor: "#454256",
+      height: 150,
+      width: "100%",
+      padding: 10
+
+    }}>
+      <Typography style={{ color: "white" }}>
+        Pecas-Projects
+        <CopyrightIcon style={{ color: "white" }} />
+      </Typography>
+
+      <br />
+
+      <Typography style={{ color: "white", fontSize: '14px' }}>Instagram - @PecasProjects</Typography>
+      <Typography style={{ color: "white", fontSize: '14px' }}>Email - PecasProjects@gmail.com</Typography>
+      <Typography style={{ color: "white", fontSize: '14px' }}>Github - https://github.com/Pecas-Projects</Typography>
+
     </footer>
   );
 }
