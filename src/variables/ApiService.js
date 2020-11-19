58f4978604421f9ccea3524a70d7636fd08c6ede
Ana Tartari <anatartari412@gmail.com>
2020-11-19 18:05:47 -0300

    ApiService

---
+++ src/variables/ApiService.js
@@ -0,0 +1,15 @@
+import api from "../services/api";
+
+const ApiService = {
+  LoginAtletica: (crecencial) => {
+    return api
+      .post("/api/Login/Atletica", crecencial)
+      .then((res) => {
+        return res;
+      })
+      .catch((error) => {
+        console.error(error);
+        return Promise.reject(error);
+      });
+  },
+};
