cf76b2dd8484e77edd0342a0254bebd9b074c861
biacalazans <biacalazans@users.noreply.github.com>
2020-11-19 20:02:52 -0300

    Atualização AdicionarProduto

--- src/Pages/AdicionarProduto/index.js
+++ src/Pages/AdicionarProduto/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../Components/NavBar";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Paper } from "@material-ui/core";
 import FormularioProduto from "./components/FormularioProduto";
 import FormularioProdutoMobile from "./components/FormularioProdutoMobile";
+import FormularioEditarProduto from "./components/FormularioEditarProduto"
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -61,6 +66,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AdicionarProduto() {
   const classes = useStyles();
+  const [opcao, setOpcao] = useState('Adicionar');
+
+  const handleChange = (e) => {
+    setOpcao(e.target.value)
+  }
 
   return (
     <div className={classes.root}>
@@ -80,10 +90,31 @@ export default function AdicionarProduto() {
         */}
 
         <div className={classes.sectionDesktop}>
+
           <Grid container justify="center">
-            <Grid container spacing={1} style={{ marginTop: 20 }}>
-              <FormularioProduto />
+            <Grid item xs={12} >
+
+              <Grid container justify='flex-start' >
+                <FormControl component="fieldset" style={{ marginLeft: 90 }}>
+                  <RadioGroup row aria-label="modalidade" name="modalidade" value={opcao} onChange={handleChange}>
+                    <FormControlLabel value="Adicionar" control={<Radio />} label="Adicionar Produto" />
+                    <FormControlLabel value="Editar" control={<Radio />} label="Editar Produto" />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
             </Grid>
+
+            {opcao === "Adicionar" ? (
+
+              <FormularioProduto />
+
+            ) : (
+
+                <FormularioEditarProduto />
+
+              )}
+
+
           </Grid>
         </div>
 
