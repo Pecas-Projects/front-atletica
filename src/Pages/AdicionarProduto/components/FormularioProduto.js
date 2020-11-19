167c59a483dcc1bef3433aae535c26e0b3a68aab
biacalazans <biacalazans@users.noreply.github.com>
2020-11-19 16:46:10 -0300

    Atualização Formulário do Produto
    Adicionando Switch de estoque

--- src/Pages/AdicionarProduto/components/FormularioProduto.js
+++ src/Pages/AdicionarProduto/components/FormularioProduto.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { AvForm, AvField } from "availity-reactstrap-validation";
-import { Grid, Typography, Paper, Button, IconButton } from "@material-ui/core";
+import { Grid, Typography, Paper, Button, Switch, FormControlLabel, TextField, MenuItem } from "@material-ui/core";
 import AddFile from "../../../assets/imagem/file-add.svg";
 import BotaoUploadImagem from "../../../Components/BotaoUploadImagem"
 
@@ -9,11 +9,15 @@ import BotaoUploadImagem from "../../../Components/BotaoUploadImagem"
 export default function FormularioProduto() {
   const [imagem, setImagem] = useState(null);
   const [path, setPath] = useState();
+  const [estoque, setEstoque] = useState(false)
+  const [ctegoria, setCategoria] = useState('')
 
-  function showAdicionarImagem(){
-    if(imagem === null){
+  const categorias = ["categoria 1", "categoria 2", "categoria 3"];
+
+  function showAdicionarImagem() {
+    if (imagem === null) {
       return <p>Adicione uma Imagem</p>
-    }else return <div><br/><br/></div>;
+    } else return <div><br /><br /></div>;
   }
 
   return (
@@ -29,19 +33,20 @@ export default function FormularioProduto() {
               <Grid container spacing={5} style={{ paddingTop: 30 }}>
                 <Grid item xs={4}>
                   {showAdicionarImagem()}
-                  <Paper style={{ backgroundColor: "#636363",width:250 }}>
+                  <Paper style={{ backgroundColor: "#636363", width: 250 }}>
                     <Grid
                       container
                       justify="center"
                       alignContent="center"
-                      style={{ height: 250, marginTop: -7 }}s
+                      style={{ height: 250, marginTop: -7 }} s
                     >
-                      <BotaoUploadImagem setPath={setPath} setImagem={setImagem} imagem={imagem} path={path}/>
+                      <BotaoUploadImagem setPath={setPath} setImagem={setImagem} imagem={imagem} path={path} />
                     </Grid>
                   </Paper>
                 </Grid>
 
                 <Grid item xs={8}>
+
                   <AvField
                     name="nome"
                     label="Nome do Produto"
@@ -69,32 +74,62 @@ export default function FormularioProduto() {
                       maxLength: { value: 45, errorMessage: "Nome inválido" },
                     }}
                   />
-                </Grid>
-                <Grid container spacing={9} style={{ paddingLeft: 20 }}>
-                  <Grid item xs={3}>
-                    <AvField name="preço" label="Preço" type="number" />
-                  </Grid>
-                  <Grid item xs={3}>
-                    <AvField name="categoria" label="Categoria" type="select">
-                      <option>Volei</option>
-                    </AvField>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Grid
-                      container
-                      justify="flex-end"
-                      style={{ paddingRight: 20 }}
-                    >
-                      <Button
-                        style={{
-                          background: "#DB4922",
-                          width: 200,
-                          marginTop: 24,
-                        }}
-                      >
-                        Postar
-                      </Button>
+
+                  <Grid container spacing={2}>
+
+                    <Grid item xs={4}>
+                      <AvField name="preço" label="Preço" type="number" />
                     </Grid>
+
+                    <Grid item xs={4}>
+
+                      <TextField
+                        fullWidth
+                        id="standard-select-categoria"
+                        select
+                        label="Categoria"
+                        style={{ marginTop: 5, marginLeft: 5 }}
+                      // value={coordenador}
+                      // onChange={handleMembroChange}
+                      >
+                        {categorias.map((option) => (
+                          <MenuItem key={option} value={option}>
+                            {option}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+
+                    </Grid>
+
+                    <Grid item xs={4}>
+
+                      <FormControlLabel
+                        style={{ marginTop: 25, marginLeft: 10 }}
+                        control={<Switch name="estoque" />}
+                        label="Em estoque"
+                      />
+
+                    </Grid>
+
+                  </Grid>
+
+                </Grid>
+
+
+                <Grid item xs={12}>
+                  <Grid
+                    container
+                    justify="flex-end"
+                  // style={{ paddingRight: 20 }}
+                  >
+                    <Button
+                      color='secondary'
+                      variant='contained'
+                      style={{ width: 300 }}
+                    >
+                      Postar
+                      </Button>
+
                   </Grid>
                 </Grid>
               </Grid>
