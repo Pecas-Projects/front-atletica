0076b5e83a42abc6fdd6a2d11bfaf8f46170b5ea
fernandanlisboa <fernandanlisboa@gmail.com>
2020-11-25 16:58:20 -0300

    Adicionando headers ao axios

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,11 @@ const url = process.env.REACT_APP_BASE_URL;
 
 const api = axios.create({
   baseURL: url,
+  config: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  }
 });
 
 export default api;
