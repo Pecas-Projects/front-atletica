461f437f96a55bdda0d1edf590870c517969764b
Davi Costa <davi.machado.costa@hotmail.com>
2020-11-26 12:40:30 -0300

    HOTFIX - Alterando a autorização para Bearer token

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@ const api = axios.create({
 api.interceptors.request.use(async (config) => {
   const token = localStorage.getItem("@Olympos:token");
   if (token) {
-    config.headers.Authorization = `token ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
