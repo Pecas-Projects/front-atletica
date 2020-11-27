a6fffc71650326b030cda50188ee3ffe4fb1c8fe
biacalazans <biacalazans@users.noreply.github.com>
2020-11-27 20:27:55 -0300

    Atualização Storage
    colocando o atleticaId no localstorage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,13 +1,21 @@
-export const login = (token, userType, userId) => {
+export const loginMembro = (token, userType, userId, atleticaId) => {
     localStorage.setItem('TOKEN', token);
     localStorage.setItem('USER_TYPE', userType);
     localStorage.setItem('USER_ID', userId);
+    localStorage.setItem('ATLETICA_ID', atleticaId);
+}
+
+export const loginAtletica = (token, userType, atleticaId) => {
+    localStorage.setItem('TOKEN', token);
+    localStorage.setItem('USER_TYPE', userType);
+    localStorage.setItem('ATLETICA_ID', atleticaId);
 }
 
 export const logout = () => {
     localStorage.removeItem('TOKEN');
     localStorage.removeItem('USER_TYPE');
     localStorage.removeItem('USER_ID');
+    localStorage.removeItem('ATLETICA_ID');
 }
 
 export const isLogin = () => {
@@ -27,4 +35,8 @@ export const getUserType = () => {
 
 export const getUserId = () => {
     return localStorage.getItem('USER_ID')
+}
+
+export const getAtleticaId = () => {
+    return localStorage.getItem('ATLETICA_ID')
 }
