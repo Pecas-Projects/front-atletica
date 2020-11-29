0146981d4e1e9ea106423a4a739b5e3401a49bae
fernandanlisboa <fernandanlisboa@gmail.com>
2020-11-29 19:58:13 -0300

    Adicionando switch do Em Estoque para adição de produto no Mobile

--- src/Pages/AdicionarProduto/components/FormularioProdutoMobile.js
+++ src/Pages/AdicionarProduto/components/FormularioProdutoMobile.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { AvForm, AvField } from "availity-reactstrap-validation";
-import { Grid, Typography, Paper, Button, IconButton, TextField, MenuItem } from "@material-ui/core";
+import { Grid, Typography, Paper, Button, Switch, FormControlLabel, TextField, MenuItem } from "@material-ui/core";
 import BotaoUploadImagemMobile from "../../../Components/BotaoUploadImagemMobile";
 import ApiService from "../../../variables/ApiService";
 import { getAtleticaId } from "../../../utils/storage";
@@ -21,43 +21,43 @@ export default function FormularioProdutoMobile() {
   });
 
   const handleCategoriaChange = (e) => {
-    setProduto({...produto, ProdutoCategoriaId: e.target.value})
+    setProduto({ ...produto, ProdutoCategoriaId: e.target.value })
   }
 
   const handleNomeChange = (e) => {
     e.preventDefault();
-    setProduto({...produto, Nome: e.target.value})
+    setProduto({ ...produto, Nome: e.target.value })
   }
 
   const handleDescricaoChange = (e) => {
     e.preventDefault();
-    setProduto({...produto, Descricao: e.target.value})
+    setProduto({ ...produto, Descricao: e.target.value })
   }
 
   const handlePrecoChange = (e) => {
     e.preventDefault();
-    setProduto({...produto, Preco: e.target.value})
+    setProduto({ ...produto, Preco: e.target.value })
   }
 
   const handleEstoqueChange = (e) => {
     e.preventDefault();
-    setProduto({...produto, Estoque: !produto.Estoque})
+    setProduto({ ...produto, Estoque: !produto.Estoque })
   };
 
   useEffect(() => {
     buscarTodasCategorias();
-    if(produto.ImagemId !== null && produto.ImagemId !== undefined)
+    if (produto.ImagemId !== null && produto.ImagemId !== undefined)
       criarProduto();
-  },[produto.ImagemId]);
+  }, [produto.ImagemId]);
 
-  async function envioImagem(){
+  async function envioImagem() {
     let file = new FormData();
     file.append('value', imagem);
 
     await ApiService.UploadImagem(file)
       .then((res) => {
         console.log(res)
-        setProduto({...produto, ImagemId: res.data.imagemId})
+        setProduto({ ...produto, ImagemId: res.data.imagemId })
       })
       .catch((error) => {
         console.log(error)
@@ -76,7 +76,7 @@ export default function FormularioProdutoMobile() {
       );
   }
 
-  async function buscarTodasCategorias(){
+  async function buscarTodasCategorias() {
     ApiService.BuscarTodasCategorias()
       .then((response) => {
         setCategorias(response.data)
@@ -86,14 +86,14 @@ export default function FormularioProdutoMobile() {
       })
   }
 
-  async function criarProduto(){
+  async function criarProduto() {
     ApiService.CriarProduto(produto)
       .then((response) => {
         console.log(response)
       })
       .catch((error) => {
         console.log(error)
-      })  
+      })
   }
 
   return (
@@ -162,22 +162,30 @@ export default function FormularioProdutoMobile() {
             <Grid item xs={12}>
               <AvField name="preço" label="Preço" type="number" onChange={handlePrecoChange} />
             </Grid>
-            <Grid item xs={12}>
-            <TextField
-                      fullWidth
-                      id="standard-select-categoria"
-                      select
-                      label="Categoria"
-                      style={{ marginTop: 5, marginLeft: 5 }}
-                      value={produto.ProdutoCategoriaId}
-                      onChange={handleCategoriaChange}
-                    >
-                      {categorias.map((option) => (
-                        <MenuItem value={option.produtoCategoriaId} key={option.nome}>
-                          {option.nome}
-                        </MenuItem>
-                      ))}
-                    </TextField>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                id="standard-select-categoria"
+                select
+                label="Categoria"
+                style={{ marginTop: 5, marginLeft: 5 }}
+                value={produto.ProdutoCategoriaId}
+                onChange={handleCategoriaChange}
+              >
+                {categorias.map((option) => (
+                  <MenuItem value={option.produtoCategoriaId} key={option.nome}>
+                    {option.nome}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControlLabel
+                style={{ marginTop: 25, marginLeft: 10 }}
+                control={<Switch name="estoque" />}
+                label="Em estoque"
+                onChange={handleEstoqueChange}
+              />
             </Grid>
             <Button
               style={{
