20805e6b4350ee6dd202bb4300bba62b56b08b83
fernandanlisboa <fernandanlisboa@gmail.com>
2020-11-29 16:56:17 -0300

    Atualização para receber atleticaId e membroId

--- src/variables/ApiService.js
+++ src/variables/ApiService.js
@@ -1,5 +1,5 @@
 import api from "../services/api";
-import { login } from "../utils/storage";
+import { login, loginAtletica, loginMembro } from "../utils/storage";
 
 const ApiService = {
 
@@ -7,7 +7,7 @@ const ApiService = {
     return api
       .post("/api/Login/Atletica", crecencial)
       .then((res) => {
-        login(res.data.token, 'A', res.data.atletica.atleticaId)
+        loginAtletica(res.data.token, 'A', res.data.atletica.atleticaId)
         return res
       })
       .catch((error) => {
@@ -62,7 +62,7 @@ const ApiService = {
     return api
       .post("/api/Login/Membro", credencial)
       .then((res) => {
-        login(res.data.token, 'A', res.data.atletica.membroId)
+        loginMembro(res.data.token, 'A', res.data.atletica.membroId, res.data.atletica.pessoa.atleticaId)
         return res;
       })
       .catch((error) => {
