70f9da6cc1daef08bb02c03aeb0f31dfa19c520c
fernandanlisboa <fernandanlisboa@gmail.com>
2020-12-01 15:50:10 -0300

    Alterando props e adicionando funções de apresentação condicional no Mobile

--- src/Pages/Times/Components/CardTimeMobile.js
+++ src/Pages/Times/Components/CardTimeMobile.js
@@ -24,25 +24,35 @@ const useStyles = makeStyles((theme) => ({
 export default function CardTime(props) {
   const { time } = props;
   const classes = useStyles();
-  const cordenador = "Coordenador: " + time.coordenador
+  const coordenador = "Coordenador: " + time.coordenador
 
-  return (
-    <>
-      <Grid container justify="center" style={{ marginBottom: 25 }}>
-        <Card color="primary" style={{ backgroundColor: "#D2CFE5" }}>
-          <CardHeader title={time.nome}  subheader={cordenador} />
-          <CardMedia
-            className={classes.imagem}
-            image={time.imagem}
-            title={time.nome}
-            style={{marginLeft:10, marginRight:10}}
-          />
-          <CardContent className={classes.info}>
-            <Typography gutterBottom style={{ color: "#020431", fontSize: 16 , paddingBottom:10}}>
+  function apresentaFoto() {
+    if (time.imagemModalidade !== null && time.imagemModalidade !== undefined) {
+      console.log(time.imagemModalidade.path)
+      return (
+        <CardMedia
+          className={classes.imagem}
+          image={time.imagemModalidade.path}
+          title={time.modalidade}
+        />
+      );
+    }
+    else {
+      return (
+        <div></div>
+      );
+    }
+  }
+
+  function apresentaTreinos() {
+    if (time.agendaTreinos !== null && time.agendaTreinos !== undefined && time.agendaTreinos.length !== 0) {
+      return (
+        <>
+          <Typography gutterBottom style={{ color: "#020431", fontSize: 16, paddingBottom: 10 }}>
               Horário dos Treinos:
-            </Typography>
-
-            {time.treinos.map((treino) => (
+          </Typography>
+          {
+            time.agendaTreinos.map((treino) => (
               <Typography
                 variant="body2"
                 component="p"
@@ -50,7 +60,33 @@ export default function CardTime(props) {
               >
                 {treino.dia} {treino.hora}
               </Typography>
-            ))}
+            ))
+          }
+        </>
+      );
+    }
+    else {
+      return (
+        <Typography gutterBottom style={{ color: "#020431", fontSize: 16, paddingBottom: 10 }}>
+              Sem treinos definidos
+        </Typography>
+      );
+    }
+  }
+
+  return (
+    <>
+      <Grid container justify="center" style={{ marginBottom: 25 }}>
+        <Card color="primary" style={{ backgroundColor: "#D2CFE5" }}>
+          <CardHeader title={time.modalidade} subheader={coordenador} />
+          <CardMedia
+            className={classes.imagem}
+            image={time.imagemModalidade.path}
+            title={time.modalidade}
+            style={{ marginLeft: 10, marginRight: 10 }}
+          />
+          <CardContent className={classes.info}>
+            {apresentaTreinos()}
           </CardContent>
         </Card>
       </Grid>
