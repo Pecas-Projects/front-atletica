d1d92fb3f1424d72e2678888229ad618fa767837
AmadoMaria <mariantoniamado@hotmail.com>
2020-12-06 15:40:22 -0300

    adicionando botão de adicionar produto

--- src/Pages/Produtos/index.js
+++ src/Pages/Produtos/index.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "../../Components/NavBar";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid, Paper } from "@material-ui/core";
-import CardProduto from "./Components/CardProduto"
-import "./styles.css"
+import { Grid, Paper, Fab, Typography } from "@material-ui/core";
+import CardProduto from "./Components/CardProduto";
+import "./styles.css";
 import ApiService from "../../variables/ApiService";
 import { getAtleticaId } from "../../utils/storage";
+import { Link } from "react-router-dom";
+import AddIcon from "@material-ui/icons/Add";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -66,21 +68,21 @@ export default function Produtos(props) {
   const username = props.match.params.username;
   const [atleticaId, setAtleticaId] = useState();
   const atleticaLoginId = getAtleticaId();
-  const [logada, setLogada] = useState(false)
+  const [logada, setLogada] = useState(false);
   const [produtos, setProdutos] = useState([]);
 
   const DeleteProduto = (index) => {
     let newArray = [...produtos];
     newArray.splice(index, 1);
-    setProdutos(newArray)
+    setProdutos(newArray);
   };
 
   useEffect(() => {
     buscaAtletica();
-    if (atleticaId !== undefined && atleticaId !== null){
+    if (atleticaId !== undefined && atleticaId !== null) {
       buscarProdutos();
-      if(atleticaLoginId === atleticaId){
-        setLogada(true)
+      if (atleticaLoginId === atleticaId) {
+        setLogada(true);
       }
     }
   }, [atleticaId, atleticaLoginId]);
@@ -88,22 +90,24 @@ export default function Produtos(props) {
   async function buscaAtletica() {
     await ApiService.PesquisaAtleticaPorUsername(username)
       .then((res) => {
-        setAtleticaId(res.data.atleticaId)
+        setAtleticaId(res.data.atleticaId);
+        
       })
       .catch((err) => {
-        console.log(err)
-      })
+        console.log(err);
+      });
   }
 
   async function buscarProdutos() {
     await ApiService.BuscarProdutosAtletica(atleticaId)
       .then((res) => {
-        console.log(res.data)
-        setProdutos(res.data)
+        console.log(res.data);
+        console.log(atleticaId)
+        setProdutos(res.data);
       })
       .catch((error) => {
-        console.log(error)
-      })
+        console.log(error);
+      });
   }
 
   return (
@@ -114,39 +118,78 @@ export default function Produtos(props) {
         <div className={classes.toolbar} />
 
         <div className={classes.sectionDesktop}>
-          <Grid container justify="center">
-            <Paper className={classes.paperA}>
-              {
-                produtos !== undefined && produtos !== null && produtos.length !== 0 ?
-                  <h4 className="MyTitle">Nossos Produtos</h4>
-                  :
-                  <h4 className="MyTitle">Essa atlética não possui produtos cadastrados!</h4>
-              }
-              <Grid container spacing={1} style={{ marginTop: 20 }}>
-                {produtos.map((item, index) => (
-                  <CardProduto item={item} atletica={logada} index={index} DeleteProduto={DeleteProduto} />
-                ))}
+          <div className="page-container-post">
+            <div className="content-wrap-post">
+              <Grid container justify="center">
+                <Paper className={classes.paperA}>
+                  {produtos !== undefined &&
+                  produtos !== null &&
+                  produtos.length !== 0 ? (
+                    <h4 className="MyTitle">Nossos Produtos</h4>
+                  ) : (
+                    <h4 className="MyTitle">
+                      Essa atlética não possui produtos cadastrados!
+                    </h4>
+                  )}
+                  <Grid container spacing={1} style={{ marginTop: 20 }}>
+                    {produtos.map((item, index) => (
+                      <CardProduto
+                        item={item}
+                        atletica={logada}
+                        index={index}
+                        DeleteProduto={DeleteProduto}
+                      />
+                    ))}
+                  </Grid>
+                </Paper>
               </Grid>
-            </Paper>
-          </Grid>
+            </div>
+          </div>
         </div>
 
         <div className={classes.sectionMobile}>
-          <Grid item xs={1}></Grid>
-          <Grid container spacing={1} style={{ marginTop: 20 }}>
-            {
-              produtos !== undefined && produtos !== null && produtos.length !== 0 ?
-                <h4 className="MyTitle" style={{ color: 'black' }}>Nossos Produtos</h4>
-                :
-                <h4 className="MyTitle" style={{ color: 'black' }}>Essa atlética não possui produtos cadastrados!</h4>
-            }
-            {produtos.map((item, index) => (
-              <CardProduto item={item} atletica={logada} index={index} DeleteProduto={DeleteProduto} />
-            ))}
-          </Grid>
-          <Grid item xs={1}></Grid>
+          <div className="page-container-post">
+            <div className="content-wrap-post">
+              <Grid container spacing={1} style={{ marginTop: 20 }}>
+                {produtos !== undefined &&
+                produtos !== null &&
+                produtos.length !== 0 ? (
+                  <h4 className="MyTitle" style={{ color: "black" }}>
+                    Nossos Produtos
+                  </h4>
+                ) : (
+                  <Grid container justify="center">
+                  <Paper className={classes.paperA}>
+                    <Typography
+                      variant="h6"
+                      align="center"
+                      style={{ color: "white" }}
+                    >
+                      Essa atlética não possui produtos cadastrados!
+                    </Typography>
+                  </Paper>
+                  </Grid>
+                )}
+                {produtos.map((item, index) => (
+                  <CardProduto
+                    item={item}
+                    atletica={logada}
+                    index={index}
+                    DeleteProduto={DeleteProduto}
+                  />
+                ))}
+              </Grid>
+            </div>
+          </div>
         </div>
 
+        <Grid container justify="flex-end">
+          <Link to="/AdicionarProduto">
+            <Fab color="secondary" aria-label="add">
+              <AddIcon />
+            </Fab>
+          </Link>
+        </Grid>
       </main>
     </div>
   );
