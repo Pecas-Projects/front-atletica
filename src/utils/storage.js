eae0e276971df2ed6f215f1a8adcf64219e84301
Davi Costa <davi.machado.costa@hotmail.com>
2020-12-06 16:52:04 -0300

    HOTFIX - Guardando o username da atlética para pesquisa ao realzar login

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -4,6 +4,7 @@ export const loginMembro = (token, userType, userId, atleticaId, username) => {
   localStorage.setItem("USER_ID", userId);
   localStorage.setItem("ATLETICA_ID", atleticaId);
   localStorage.setItem("ATLETICA_USERNAME", username);
+  localStorage.setItem("ATLETICA_USERNAME_PESQUISADA", username);
 };
 
 export const loginAtletica = (token, userType, atleticaId, username) => {
@@ -11,6 +12,7 @@ export const loginAtletica = (token, userType, atleticaId, username) => {
   localStorage.setItem("USER_TYPE", userType);
   localStorage.setItem("ATLETICA_ID", atleticaId);
   localStorage.setItem("ATLETICA_USERNAME", username);
+  localStorage.setItem("ATLETICA_USERNAME_PESQUISADA", username);
 };
 
 export const logout = () => {
