adfe8a6f4c0cf6e2b5f3c195b0c0f9c07a3c1edd
fernandanlisboa <fernandanlisboa@gmail.com>
2020-12-06 15:26:24 -0300

    Adicionando defaultValues no avForm e loading da pagina

--- src/Pages/EditarProduto/Components/FormularioEditarProduto.js
+++ src/Pages/EditarProduto/Components/FormularioEditarProduto.js
@@ -30,24 +30,26 @@ export default function FormularioProduto(props) {
     const [preco, setPreco] = useState('')
     const [nome, setNome] = useState('')
     const [produto, setProduto] = useState({
-        ProdutoId: props.produtoId,
-        Nome: "",
-        Descricao: "",
-        Preco: "",
-        ProdutoCategoriaId: undefined,
-        Estoque: false,
-        AtleticaId: getAtleticaId(),
-        ImagemId: null,
-        Imagem: {
-            Extensao: "",
-            Path: "",
-            ImagemId: null
+        produtoId: props.produtoId,
+        nome: "",
+        descricao: "",
+        preco: "",
+        produtoCategoriaId: undefined,
+        estoque: false,
+        atleticaId: getAtleticaId(),
+        imagemId: null,
+        imagem: {
+            extensao: "",
+            path: "",
+            imagemId: null
         }
     });
 
     const defaultValues = {
-        
-    }
+        nome: produto.nome,
+        descricao: produto.descricao,
+        preco: produto.preco,
+    };
 
     const handleCategoriaChange = (e) => {
         setCategoria(e.target.value)
@@ -77,9 +79,11 @@ export default function FormularioProduto(props) {
     }, []);
 
     async function buscarProduto() {
-        await ApiService.BuscarProdutoId(produto.ProdutoId)
+        await ApiService.BuscarProdutoId(produto.produtoId)
             .then((res) => {
                 console.log(res.data)
+                setImagem(res.data.imagem)
+                setPath(res.data.imagem.path)
                 setProduto(res.data)
             })
             .catch((err) => {
@@ -96,111 +100,114 @@ export default function FormularioProduto(props) {
     return (
         <>
             <Grid container justify="center" style={{ marginBottom: 25 }}>
+                {
+                    produto.produtoCategoriaId === undefined ?
+                        <h4>Loading...</h4>
+                        :
+                        <Paper className={classes.paperA}>
 
+                            <h4 className="MyTitle">Editar Produto</h4>
 
-                <Paper className={classes.paperA}>
+                            <Typography variant="h8" style={{ color: "#454256" }}>
+                                Edite um produto da aba produtos da sua atlética
+                    </Typography>
 
-                    <h4 className="MyTitle">Editar Produto</h4>
-
-                    <Typography variant="h8" style={{ color: "#454256" }}>
-                        Edite um produto da aba produtos da sua atlética
-            </Typography>
-
-                    <AvForm model={defaultValues}>
-                        <Grid container spacing={1} style={{ paddingTop: 20 }}>
-                            <Grid item xs={4}>
-
-                                {showAdicionarImagem()}
-                                <Paper style={{ backgroundColor: "#636363", width: 250 }}>
-                                    <Grid
-                                        container
-                                        justify="center"
-                                        alignContent="center"
-                                        style={{ height: 250, marginTop: -7 }}
-                                    >
-                                        <BotaoUploadImagem setPath={setPath} setImagem={setImagem} imagem={imagem} path={path} />
-                                    </Grid>
-                                </Paper>
-                            </Grid>
-
-                            <Grid item xs={8}>
-
-                                <AvField
-                                    name="nome"
-                                    label="Nome do Produto"
-                                    type="text"
-                                    disabled
-                                    style={{ color: "E2E2E2" }}
-                                />
-                                <AvField
-                                    name="descrição"
-                                    label="Descrição"
-                                    type="textarea"
-                                    disabled
-                                />
-
-                                <Grid container spacing={2}>
-
-                                    <Grid item xs={4}>
-                                        <AvField name="preço" label="Preço" type="number" value={produto.Preco} />
-                                    </Grid>
-
+                            <AvForm model={defaultValues}>
+                                <Grid container spacing={1} style={{ paddingTop: 20 }}>
                                     <Grid item xs={4}>
 
-                                        <TextField
-                                            fullWidth
-                                            id="standard-select-categoria"
-                                            select
-                                            label="Categoria"
-                                            style={{ marginTop: 5, marginLeft: 5 }}
-                                            value={produto.ProdutoCategoriaId}
-                                            onChange={handleCategoriaChange}
+                                        {showAdicionarImagem()}
+                                        <Paper style={{ backgroundColor: "#636363", width: 250 }}>
+                                            <Grid
+                                                container
+                                                justify="center"
+                                                alignContent="center"
+                                                style={{ height: 250, marginTop: -7 }}
+                                            >
+                                                <BotaoUploadImagem setPath={setPath} setImagem={setImagem} imagem={imagem} path={path} />
+                                            </Grid>
+                                        </Paper>
+                                    </Grid>
+
+                                    <Grid item xs={8}>
+
+                                        <AvField
+                                            name="nome"
+                                            label="Nome do Produto"
+                                            type="text"
                                             disabled
-                                        >
-                                            {categorias.map((option) => (
-                                                <MenuItem value={option.produtoCategoriaId} key={option.nome}>
-                                                    {option.nome}
-                                                </MenuItem>
-                                            ))}
-                                        </TextField>
-
-                                    </Grid>
-
-                                    <Grid item xs={4}>
-
-                                        <FormControlLabel
-                                            style={{ marginTop: 25, marginLeft: 10 }}
-                                            control={<Switch name="estoque" />}
-                                            label="Em estoque"
-                                            onChange={handleEstoqueChange}
+                                            style={{ color: "E2E2E2" }}
+                                        />
+                                        <AvField
+                                            name="descricao"
+                                            label="Descrição"
+                                            type="textarea"
+                                            disabled
                                         />
 
+                                        <Grid container spacing={2}>
+
+                                            <Grid item xs={4}>
+                                                <AvField name="preco" label="Preço" type="number" value={produto.Preco} />
+                                            </Grid>
+
+                                            <Grid item xs={4}>
+
+                                                <TextField
+                                                    fullWidth
+                                                    id="standard-select-categoria"
+                                                    select
+                                                    label="Categoria"
+                                                    style={{ marginTop: 5, marginLeft: 5 }}
+                                                    value={produto.ProdutoCategoriaId}
+                                                    onChange={handleCategoriaChange}
+                                                    disabled
+                                                >
+                                                    {categorias.map((option) => (
+                                                        <MenuItem value={option.produtoCategoriaId} key={option.nome}>
+                                                            {option.nome}
+                                                        </MenuItem>
+                                                    ))}
+                                                </TextField>
+
+                                            </Grid>
+
+                                            <Grid item xs={4}>
+
+                                                <FormControlLabel
+                                                    style={{ marginTop: 25, marginLeft: 10 }}
+                                                    control={<Switch checked={produto.Estoque} name="estoque" />}
+                                                    label="Em estoque"
+                                                    onChange={handleEstoqueChange}
+                                                />
+
+                                            </Grid>
+
+                                        </Grid>
+
                                     </Grid>
 
+
+                                    <Grid item xs={12}>
+                                        <Grid
+                                            container
+                                            justify="flex-end"
+                                        // style={{ paddingRight: 20 }}
+                                        >
+                                            <Button
+                                                color='secondary'
+                                                variant='contained'
+                                                style={{ width: 300 }}
+                                            >
+                                                Postar
+                                            </Button>
+
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
-
-                            </Grid>
-
-
-                            <Grid item xs={12}>
-                                <Grid
-                                    container
-                                    justify="flex-end"
-                                // style={{ paddingRight: 20 }}
-                                >
-                                    <Button
-                                        color='secondary'
-                                        variant='contained'
-                                        style={{ width: 300 }}
-                                    >
-                                        Postar
-                      </Button>
-
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </AvForm>
-                </Paper>
+                            </AvForm>
+                        </Paper>
+                }
             </Grid>
 
         </>
