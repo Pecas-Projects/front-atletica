6c74a294976070b921280510860208f3d066f75d
biacalazans <biacalazans@users.noreply.github.com>
2020-12-07 19:15:30 -0300

    Atualização PaperNotificacoes
    colocando alerts

--- src/Pages/Notificacoes/Components/PaperNotificacao.js
+++ src/Pages/Notificacoes/Components/PaperNotificacao.js
@@ -7,12 +7,17 @@ import {
   Paper,
   Typography,
   makeStyles,
+  Snackbar
 } from "@material-ui/core";
 import "../styles.css";
 
 import atleta_icon from "../../../assets/imagem/atleta_icon.svg";
 import jogo_icon from "../../../assets/imagem/jogo_icon.svg";
-import { yellow } from "@material-ui/core/colors";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -65,48 +70,75 @@ function PaperNotificacao(props) {
   const classes = useStyles();
   const { item } = props;
 
+  const [openAceito, setOpenAceito] = useState(false)
+  const [openRecusado, setOpenRecusado] = useState(false)
+
+  const handleCloseAceito = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenAceito(false);
+  };
+
+  const handleCloseRecusado = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenRecusado(false);
+  };
+
   async function aprovaSolicitacaoAtleta(solicitacaoAtletaId) {
     await ApiService.AprovarSolicitacaoAtleta(solicitacaoAtletaId)
       .then((res) => {
         console.log(res);
+        setOpenAceito(true)
+        setTimeout(function () { window.location.href = '/notificacoes' }, 3000)
       })
       .catch((error) => {
         console.log(error);
       });
-      props.getSolicitacoes()
+    props.getSolicitacoes()
   }
 
   async function reprovaSolicitacaoAtleta(solicitacaoAtletaId) {
     await ApiService.ReprovarSolicitacaoAtleta(solicitacaoAtletaId)
       .then((res) => {
         console.log(res);
+        setOpenRecusado(true)
+        setTimeout(function () { window.location.href = '/notificacoes' }, 3000)
       })
       .catch((error) => {
         console.log(error);
       });
-      props.getSolicitacoes()
+    props.getSolicitacoes()
   }
 
   async function aprovaSolicitacaoJogo(solicitacaoJogoId) {
     await ApiService.AprovarSolicitacaoJogo(solicitacaoJogoId)
       .then((res) => {
         console.log(res);
+        setOpenAceito(true)
+        setTimeout(function () { window.location.href = '/notificacoes' }, 3000)
       })
       .catch((error) => {
         console.log(error);
       });
-      props.getSolicitacoes()
+    props.getSolicitacoes()
   }
 
   async function reprovaSolicitacaoJogo(solicitacaoJogoId) {
     await ApiService.ReprovarSolicitacaoJogo(solicitacaoJogoId)
       .then((res) => {
         console.log(res);
+        setOpenRecusado(true)
+        setTimeout(function () { window.location.href = '/notificacoes' }, 3000)
       })
       .catch((error) => {
         console.log(error);
       });
-      props.getSolicitacoes()
+    props.getSolicitacoes()
   }
 
   function handleAceitarSolicitacao() {
@@ -148,7 +180,7 @@ function PaperNotificacao(props) {
           </Typography>
         </>
       );
-    } else if(props.tipo === "jogos"){
+    } else if (props.tipo === "jogos") {
       return (
         <>
           <Typography gutterBottom style={{ fontSize: 18 }}>
@@ -171,6 +203,26 @@ function PaperNotificacao(props) {
 
   return (
     <>
+      <Snackbar
+        open={openAceito}
+        autoHideDuration={4000}
+        onClose={handleCloseAceito}
+      >
+        <Alert onClose={handleCloseAceito} severity="success">
+          Solicitação aceita com sucesso!
+        </Alert>
+      </Snackbar>
+
+      <Snackbar
+        open={openRecusado}
+        autoHideDuration={4000}
+        onClose={handleCloseRecusado}
+      >
+        <Alert onClose={handleCloseRecusado} severity="success">
+          Solicitação recusada com sucesso!
+        </Alert>
+      </Snackbar>
+
       <Grid item style={{ marginBottom: 20 }} xs={12}>
         <Paper className={classes.paper}>
           <Grid container>
