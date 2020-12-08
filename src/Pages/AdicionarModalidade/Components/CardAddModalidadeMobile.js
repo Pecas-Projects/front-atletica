3f853085019b166f5b0d64929e32ce75f826901c
biacalazans <biacalazans@users.noreply.github.com>
2020-12-08 11:00:39 -0300

    Removendo warnings

--- src/Pages/AdicionarModalidade/Components/CardAddModalidadeMobile.js
+++ src/Pages/AdicionarModalidade/Components/CardAddModalidadeMobile.js
@@ -1,19 +1,6 @@
-import React, { useState } from "react";
-import { Grid, Paper, Button, TextField, MenuItem, Snackbar } from "@material-ui/core";
-import { AvForm, AvField } from 'availity-reactstrap-validation';
+import React from "react";
+import { Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import BotaoUploadImagem from "../../../Components/BotaoUploadImagem"
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import MuiAlert from '@material-ui/lab/Alert';
-import "../styles.css"
-
-function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
 
 
 const useStyles = makeStyles((theme) => ({
@@ -33,31 +20,6 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-const membros = [
-    {
-        value: 'Beatriz Calazans',
-    },
-    {
-        value: 'Fernanda Lisboa',
-    },
-    {
-        value: 'Maria Antônia',
-    },
-    {
-        value: 'Ana Paula',
-    },
-    {
-        value: 'Davi Costa',
-    },
-]
-
-const modalidades = [
-    "Futebol", "Vôlei", "Basquete", "Atletismo", "Futsal", "Vôlei de Praia", "Natação", "Outro"
-]
-
-const generos = [
-    "Feminino", "Masculino", "Misto"
-]
 
 export default function CardAddModalidade() {
 
